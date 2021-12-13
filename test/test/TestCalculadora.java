64722cde51e9c939d894e74dd0c9919356805424
EdwinCuaran <edwin.cuaran@correounivalle.edu.co>
2021-12-13 17:52:33 -0500

    Primera version de ejemplo JUnit en Java

---
+++ test/test/TestCalculadora.java
@@ -0,0 +1,36 @@
+
+package test;
+
+import org.junit.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import pruebasjunit.Calculadora;
+
+public class TestCalculadora {
+    
+    Calculadora p;
+    
+    public TestCalculadora(){
+        p = new Calculadora();
+    }
+    
+    @Test
+    public void testSumar(){
+        assertTrue(p.sumar(4,5) == 4+5);
+    }
+    
+    @Test
+    public void testRestar(){
+        assertTrue(p.restar(10,6) == 10-6);
+    }
+    
+    @Test
+    public void testMultiplicar(){
+        assertTrue(p.multiplicar(11, 10) == 11*10);
+    }
+    
+    @Test
+    public void testDividir(){
+        assertTrue(p.dividir(100,0) == 0);
+    }
+}
